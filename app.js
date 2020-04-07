34e1fe8dd8f3439503fbab9de6a86ea8f86ef940
Tbetcha <troy.boettger@gmail.com>
2020-04-06 20:27:57 -0400

    add expense and income function expressions

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
 var budgetController = (function () {
+    //function constructor cap letter in beginning
+    var Expense = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
 
+    var Income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
 
+    var allExpenses = [];
+    var allIncome = [];
+    var totalExpenses = 0;
+
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    }
 })();
 
 
